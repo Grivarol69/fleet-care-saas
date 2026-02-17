0705caf2fb29d3cdc7cdd9a538ba4a70ab5665a1
Guillermo Rivarola <grivarol69@gmail.com>
2026-02-17 18:24:51 -0500

    Fix: Force dynamic on dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default async function DashboardLayout({
   children,
 }: {
